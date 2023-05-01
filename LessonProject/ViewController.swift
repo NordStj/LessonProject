16f2b112a637f3d6ce0bf4b64354ac9d82bd00b2
NordStj <Neoskyping@gmail.com>
2023-05-01 14:03:38 +0300

    Initial Commit

---
+++ LessonProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonProject
+//
+//  Created by Дмитрий on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
